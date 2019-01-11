d8a6f8b29c03b51483b1c15d486fcd1eb20c48fa
xVan Turing <xVanTuring@outlook.com>
2019-01-11 20:35:59 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# CQuant
+> in dev
+---
+xVan Turing 2019
--- addon.js
+++ addon.js
@@ -1,13 +1,11 @@
 const addon = require('bindings')('cquant')
 const sharp = require('sharp')
-// console.log(addon)
 sharp('./img/0.png')
   .raw()
   .toBuffer().then(buffer => {
-    //  console.log(buffer[0])
     var time = Date.now()
     let data = addon.GetPalette(buffer, 8)
-    console.log(Date.now() - time, 'ms')
+    console.log(Date.now() - time + 'ms')
     console.log(data)
   }).catch(err => {
     console.log(err)
