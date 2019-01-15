d0264f278b2f4383ffa180aac6b5346a985b52d7
xVan Turing <xVanTuring@outlook.com>
2019-01-15 19:27:59 +0800

    add electron pre build script

--- script/build_deploy.js
+++ script/build_deploy.js
@@ -1,10 +1,19 @@
-const allVersion = ["6.14.2", "8.10.0", "9.11.0", "10.0.0", "11.0.0"]
+// NODE: support lts version and latest version
+// electron: 
+const allVersion = [
+  { r: "node", t: "6.14.2" },
+  { r: "node", t: "8.10.0" },
+  { r: "node", t: "10.0.0" },
+  { r: "node", t: "11.0.0" },
+  { r: "electron", t: "3.0.0" },
+  { r: "electron", t: "4.0.0" },
+]
 const spawn = require('child_process').spawn
 const async = require('async')
 const which = require('which')
-let buildQueue = async.queue((version, callback) => {
-  console.log(`Builing: ${version}`)
-  let argv = ['-t', version, '--backend', 'cmake-js']
+let buildQueue = async.queue(({ r, t }, callback) => {
+  console.log(`Building: ${r} ${t}`)
+  let argv = ['-r', r, '-t', t, '--backend', 'cmake-js']
   which('prebuild', (err, filePath) => {
     if (err) {
       callback(err)
